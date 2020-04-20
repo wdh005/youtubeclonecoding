2ec642ef727556956ecfb2f59e6840fe436484e9
Jonghyun <wdh008@naver.com>
2020-04-20 16:46:52 +0900

    start

---
+++ client/src/components/App.js
@@ -0,0 +1,34 @@
+import React, { Suspense } from 'react';
+import { Route, Switch } from "react-router-dom";
+import Auth from "../hoc/auth";
+// pages for this product
+import LandingPage from "./views/LandingPage/LandingPage.js";
+import LoginPage from "./views/LoginPage/LoginPage.js";
+import RegisterPage from "./views/RegisterPage/RegisterPage.js";
+import NavBar from "./views/NavBar/NavBar";
+import Footer from "./views/Footer/Footer";
+import VideoUploadPage from "./views/VideoUploadPage/VideoUploadPage";
+
+//null   Anyone Can go inside
+//true   only logged in user can go inside
+//false  logged in user can't go inside
+
+function App() {
+  return (
+    <Suspense fallback={(<div>Loading...</div>)}>
+      <NavBar />
+      <div style={{ paddingTop: '69px', minHeight: 'calc(100vh - 80px)' }}>
+        <Switch>
+          {/* null - 누구나 들어갈 수 있다, false - 로그인한 사람은 접근 불가, true - 로그인 한 사람만 접근 가능  */}
+          <Route exact path="/" component={Auth(LandingPage, null)} />
+          <Route exact path="/login" component={Auth(LoginPage, false)} />
+          <Route exact path="/register" component={Auth(RegisterPage, false)} />
+          <Route exact path="/video/upload" component={Auth(VideoUploadPage, true)} />
+        </Switch>
+      </div>
+      <Footer />
+    </Suspense>
+  );
+}
+
+export default App;
---
+++ client/src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -0,0 +1,184 @@
+import React, { useState } from 'react';
+import { Typography, Button, Form, message, Input, Icon } from 'antd';
+import Dropzone from 'react-dropzone';
+import Axios from 'axios';
+import { useSelector } from 'react-redux';
+
+
+const { TextArea } = Input;
+const { Title } = Typography;
+
+const PrivateOptions = [
+    {value: 0, label: "Private"},
+    {value: 1, label: "Public"}
+]
+
+const CategoryOptions = [
+    {value: 0, label: "Film & Animation"},
+    {value: 1, label: "Auto & Vehicles"},
+    {value: 2, label: "Music"},
+    {value: 3, label: "Pets & Animals"}
+]
+
+function VideoUploadPage() {
+    const user = useSelector(state => state.user);
+    const [VideoTitle, setVideoTitle] = useState("")
+    const [Description, setDescription] = useState("")
+    const [Private, setPrivate] = useState(0)
+    const [Category, setCategory] = useState("Film & Animation")
+    const [FilePath, setFilePath] = useState("")
+    const [Duration, setDuration] = useState("")
+    const [ThumbnailPath, setThumbnailPath] = useState("")
+
+    const onTitleChange = (e) => {
+        setVideoTitle(e.currentTarget.value)
+    }
+
+    const onDescriptionChange = (e) => {
+        setDescription(e.currentTarget.value)
+    }
+
+    const onPrivateChange = (e) => {
+        setPrivate(e.currentTarget.value)
+    }
+
+    const onCategoryChange = (e) => {
+        setCategory(e.currentTarget.value)
+    }
+
+    const onDrop = (files) => {
+
+        let formData = new FormData;
+        const config = {
+            header: { 'content-type': 'multipart/form-data' }
+        }
+        console.log(files)
+        formData.append("file", files[0])
+
+        Axios.post('/api/video/uploadfiles', formData, config)
+            .then(response => {
+                if (response.data.success) {
+
+                    let variable = {
+                        url: response.data.url,
+                        fileName: response.data.fileName
+                    }
+                    setFilePath(response.data.url)
+
+                    //gerenate thumbnail with this filepath ! 
+
+                    Axios.post('/api/video/thumbnail', variable)
+                        .then(response => {
+                            if (response.data.success) {
+                                setDuration(response.data.fileDuration)
+                                setThumbnailPath(response.data.url)
+                            } else {
+                                alert('썸네일 생성 실패');
+                            }
+                        })
+
+
+                } else {
+                    alert('비디오 업로드 실패')
+                }
+            })
+
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+
+        const variable = {
+            writer: user.userData._id,
+            title: VideoTitle,
+            description: Description,
+            privacy: Private,
+            filePath: FilePath,
+            category: Category,
+            duration: Duration,
+            thumbnail: ThumbnailPath
+        }
+
+        Axios.post('/api/video/uploadVideo', variable)
+        .then(response => {
+            if(response.data.success) {
+
+            } else {
+                alert('비디오 업로드에 실패 했습니다.')
+            }
+        })
+    }
+
+    return (
+        <div style={{ maxWidth:'700px', margin:'2rem auto' }}>
+            <div style={{ textAlign:'center', marginBottom:'2rem' }}>
+                <Title level={2}>Upload Video</Title>
+            </div>
+
+            <Form onSubmit={onSubmit}>
+                <div style={{ display:'flex', justifyContent:'space-between'}}>
+                    {/** Drop zone */}
+                    <Dropzone 
+                    onDrop={onDrop}
+                    multiple={false}
+                    maxSize={10000000000}
+                    >
+                    {({ getRootProps, getInputProps}) => (
+                        <div style={{ width:'300px', height:'240px', border:'1px solid lightgray', display:'flex',
+                        alignItems:'center', justifyContent:'center'}} {...getRootProps()}>
+                            <input {...getInputProps()} />
+                            <Icon type="plus" style={{ fontSize:'3rem'}} />
+                        </div>
+                    )}
+                    </Dropzone>
+
+                    {/** Thumbnail zone */}
+
+                    {ThumbnailPath && 
+                    <div>
+                        <img src={`http://localhost:5000/${ThumbnailPath}`} alt="thumbnail" />
+                    </div>
+                    }   
+                </div>
+            <br />
+            <br />
+            <label>Title</label>
+            <Input
+                onChange={onTitleChange}
+                value={VideoTitle}
+            />
+            <br />
+            <br />
+            <label>Description</label>
+            <TextArea 
+                onChange={onDescriptionChange}
+                value={Description}
+            />
+            <br />
+            <br />
+            <select onchange={onPrivateChange}>
+                {PrivateOptions.map((item, index) => (
+                    <option key={index} value={item.value}>{item.label}</option>
+
+                ))}
+            </select>
+            <br />
+            <br />
+            <select onchange={onCategoryChange}>
+            {CategoryOptions.map((item, index) => (
+                    <option key={index} value={item.value}>{item.label}</option>
+
+                ))}
+            </select>
+            <br />
+            <br />
+            <Button type="primary" size="large" onClick={onSubmit}>
+                Submit
+            </Button>
+
+            </Form>
+        </div>
+    )
+}
+
+export default VideoUploadPage
